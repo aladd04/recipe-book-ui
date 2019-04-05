7627cab8e8702c9894d2b4a76d0a8d6f45c83ff2
Andy Ladd <aladd04@gmail.com>
2019-04-05 15:50:12 -0400

    Added live API URL.

--- src/Hooks/serviceConfig.js
+++ src/Hooks/serviceConfig.js
@@ -1,1 +1,2 @@
-export const ApiUrl = "http://localhost:51301/api";
+//export const ApiUrl = "http://localhost:51301/api";
+export const ApiUrl = "https://humjtzo6s3.execute-api.us-east-1.amazonaws.com/Prod/api";
